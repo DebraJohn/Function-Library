9131d2a26498db2e556eae4b676814209cc77f0e
DebraJohn <debra.zhuang@zeasn.com>
2019-07-31 14:04:03 +0800

    add one method

--- random.js
+++ random.js
@@ -1,4 +1,7 @@
-// 从数组中随机抽取n个
+
+const arr = [2, 4, 32, 1, 43, 5, 6, 7, 134, 34, 25, 9];
+
+// 方法一 从数组中随机抽取n个
 function random(arr, amount) {
   const tempArray = [];
   for (let j = 0; j < arr.length; j++) {
@@ -12,4 +15,19 @@ function random(arr, amount) {
     tempArray.splice(index, 1);
   }
   return newArray;
+}
+
+// 方法二
+const randomArr = (arr, amount) => {
+  const arrCopy = JSON.parse(JSON.stringify(arr))
+  const newArr = []
+  let i = amount;
+  while(arrCopy.length && i > 0) {
+    const randomNum = arrCopy[~~(Math.random() * arrCopy.length)]
+    newArr.push(randomNum)
+    const index = arrCopy.findIndex(num => num === randomNum)
+    index > -1 && arrCopy.splice(index, 1)
+    i--
+  }
+  return newArr
 }
